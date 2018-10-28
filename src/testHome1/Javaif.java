775c059100b6ab7c3a5646da6e83976123952e29
jalal8068 <jalalahamad9@gmail.com>
2018-10-28 11:04:50 +0530

    2nd commit

---
+++ src/testHome1/Javaif.java
@@ -0,0 +1,19 @@
+package testHome1;
+
+public class Javaif {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=1;
+		int j=2;
+		if(i==5)
+		{
+			System.out.println("Jalal");
+		}
+		else if(j==2)
+		{
+		System.out.println("Scooby doo");
+		}
+	}
+
+}
---
+++ src/testHome1/Wordtest.java
@@ -0,0 +1,14 @@
+package testHome1;
+
+public class Wordtest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String a = "apple";
+String b ="apple";
+System.out.println(a.equals(b));
+
+
+	}
+
+}
